2f1d81124bcd2d00c123964306084ce8f19ed3ea
sonavbinu <sonavbinu567@gmail.com>
2025-09-19 14:36:11 +0530

    updated todo with startdate

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -4,19 +4,22 @@ import './TodoList.scss';
 interface Task {
   title: string;
   details: string;
+  startDate: string;
 }
 
 const TodoList: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
+  const [startDate, setStartDate] = useState('');
 
   const addTask = () => {
     if (title.trim() === '' && details.trim() === '') return;
-    const newTask: Task = { title, details };
+    const newTask: Task = { title, details, startDate };
     setTasks([...tasks, newTask]);
     setTitle('');
     setDetails('');
+    setStartDate('');
   };
 
   return (
@@ -36,15 +39,26 @@ const TodoList: React.FC = () => {
           onChange={e => setDetails(e.target.value)}
           placeholder="Details of your task"
         />
+        <input
+          className="input-date"
+          type="date"
+          value={startDate}
+          onChange={e => setStartDate(e.target.value)}
+        />
         <button onClick={addTask}>+</button>
       </div>
       <div className="todo-list">
         <ul>
           {tasks.map((task, index) => (
-            <li key={index}>
-              <strong className="title">{task.title}</strong>
+            <li key={index} className="task-card">
+              <h3>{task.title}</h3>
               <br />
-              <p className="details"> {task.details}</p>
+              <p> {task.details}</p>
+              <span>Start date:{task.startDate}</span>
+
+              <div className="task-actions">
+                <button>✏️</button>
+              </div>
             </li>
           ))}
         </ul>
