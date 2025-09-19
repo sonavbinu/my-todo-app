2d79d8d38f27faf044b9a6c8c0da4e223adfaf61
sonavbinu <sonavbinu567@gmail.com>
2025-09-19 14:19:21 +0530

    updated todolist scss

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoList.scss';
 
+interface Task {
+  title: string;
+  details: string;
+}
+
 const TodoList: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+
+  const addTask = () => {
+    if (title.trim() === '' && details.trim() === '') return;
+    const newTask: Task = { title, details };
+    setTasks([...tasks, newTask]);
+    setTitle('');
+    setDetails('');
+  };
+
   return (
-    <div>
+    <div className="todo-container">
       <div className="input-main">
         <input
           className="input-title"
           type="text"
-          placeholder="Type title of Task"
+          placeholder="Enter your task"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
         />
         <input
           className="input-details"
           type="text"
+          value={details}
+          onChange={e => setDetails(e.target.value)}
           placeholder="Details of your task"
         />
-        <button>+</button>
+        <button onClick={addTask}>+</button>
+      </div>
+      <div className="todo-list">
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <strong className="title">{task.title}</strong>
+              <br />
+              <p className="details"> {task.details}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
